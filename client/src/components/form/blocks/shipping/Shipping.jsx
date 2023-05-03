5581bfd795634a84ec9cf2ffec8c45322c17b26c
Julia Panko <juliemayak@gmail.com>
2023-05-03 10:38:53 +0700

    Add form animation (gsap)

--- client/src/components/form/blocks/shipping/Shipping.jsx
+++ client/src/components/form/blocks/shipping/Shipping.jsx
@@ -1,10 +1,42 @@
 import { Checkbox, FormControlLabel } from "@mui/material";
 import AddressForm from "../address-form/AddressForm";
+import { useRef } from "react";
+import gsap from "gsap";
 import "./shipping.scss";
 
 const Shipping = ({ values, errors, touched, handleBlur, handleChange, setFieldValue }) => {
+  const shippingFormRef = useRef();
+
+  function toggleShippingForm() {
+    const form = shippingFormRef.current;
+    const tl = gsap.timeline();
+    if (values.shippingAddress.isSameAddress) {
+      tl.fromTo(
+        form,
+        {
+          height: 0,
+        },
+        {
+          duration: 0.7,
+          height: "auto",
+          ease: "power2.out",
+        }
+      );
+      tl.fromTo(form, { autoAlpha: 0 }, { autoAlpha: 1, duration: 0.5, ease: "sine.out" }, 0.1);
+    } else {
+      tl.to(form, {
+        height: 0,
+        duration: 0.3,
+        ease: "power1.out",
+        overwrite: true,
+      });
+      tl.to(form, { autoAlpha: 0, duration: 0.2, overwrite: false }, 0);
+    }
+  }
+
   function handleCheckboxChange() {
     setFieldValue("shippingAddress.isSameAddress", !values.shippingAddress.isSameAddress);
+    toggleShippingForm();
   }
 
   return (
@@ -35,7 +67,8 @@ const Shipping = ({ values, errors, touched, handleBlur, handleChange, setFieldV
       </div>
 
       <div
-        className={`shipping__form ${values.shippingAddress.isSameAddress ? "_closed" : "_open"}`}
+        ref={shippingFormRef}
+        className={`shipping__form ${values.shippingAddress.isSameAddress ? "_closed" : ""}`}
       >
         <p className="shipping__title">Shipping Information</p>
         <AddressForm

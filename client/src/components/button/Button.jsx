77b7e81a8b64625e59f0067b6e07e35abc421424
Julia Panko <juliemayak@gmail.com>
2023-05-03 09:47:01 +0700

    Modify Button

--- client/src/components/button/Button.jsx
+++ client/src/components/button/Button.jsx
@@ -1,8 +1,8 @@
 import "./button.scss";
 
-const Button = ({ text, className, onClick }) => {
+const Button = ({ text, className, onClick, type }) => {
   return (
-    <button className={`button ${className}`} onClick={onClick}>
+    <button type={type} className={`button ${className ? className : ""}`} onClick={onClick}>
       {text}
     </button>
   );
